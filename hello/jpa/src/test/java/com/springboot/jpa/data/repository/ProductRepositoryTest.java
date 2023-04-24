0e23ac04d10ef5a14c530da44b656e14d5b9d510
jun <alap_u@naver.com>
2023-04-24 16:26:33 +0900

    chap06 Test

---
+++ hello/jpa/src/test/java/com/springboot/jpa/data/repository/ProductRepositoryTest.java
@@ -0,0 +1,12 @@
+package com.springboot.jpa.data.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+class ProductRepositoryTestByH2 {
+
+
+
+}
--- hello/jpa/src/test/java/com/springboot/jpa/service/impl/ProductServiceTest.java
+++ hello/jpa/src/test/java/com/springboot/jpa/service/impl/ProductServiceTest.java
@@ -1,17 +1,66 @@
 package com.springboot.jpa.service.impl;
 
+import static org.mockito.AdditionalAnswers.returnsFirstArg;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+
+import com.springboot.jpa.data.dto.ProductDto;
+import com.springboot.jpa.data.dto.ProductResponseDto;
+import com.springboot.jpa.data.entity.Product;
 import com.springboot.jpa.data.repository.ProductRepository;
+import java.util.Optional;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
-class ProductServiceImplTest {
+class ProductServiceTest {
 
     @InjectMocks
     private ProductServiceImpl productService;
     @Mock
     private ProductRepository productRepository;
+
+    @Test
+    void getProduct() {
+        Product givenProduct = new Product();
+        givenProduct.setNumber(123L);
+        givenProduct.setName("펜");
+        givenProduct.setPrice(1000);
+        givenProduct.setStock(1234);
+
+        given(productRepository.findById(123L))
+                .willReturn(Optional.of(givenProduct));
+
+        ProductResponseDto productResponseDto = productService.getProduct(123L);
+
+        Assertions.assertEquals(productResponseDto.getNumber(), givenProduct.getNumber());
+        Assertions.assertEquals(productResponseDto.getName(), givenProduct.getName());
+        Assertions.assertEquals(productResponseDto.getPrice(), givenProduct.getPrice());
+        Assertions.assertEquals(productResponseDto.getStock(), givenProduct.getStock());
+
+        verify(productRepository).findById(123L);
+    }
+
+    @Test
+    void saveProductTest() {
+        given(productRepository.save(any(Product.class)))
+                .willAnswer(returnsFirstArg());
+
+        ProductResponseDto productResponseDto
+                = productService.saveProduct(new ProductDto("펜", 1000, 1234));
+
+        Assertions.assertEquals(productResponseDto.getName(), "펜");
+        Assertions.assertEquals(productResponseDto.getPrice(), 1000);
+        Assertions.assertEquals(productResponseDto.getStock(), 1234);
+
+        verify(productRepository).save(any(Product.class));
+    }
+
+
 
 }
