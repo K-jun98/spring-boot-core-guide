da0ed1f2617ece1f9cb9815484978662cbb1ff83
jun <alap_u@naver.com>
2023-04-24 16:32:41 +0900

    chap08 init

---
+++ hello/adanvced_jpa/src/main/java/com/springboot/adanvced_jpa/AdanvcedJpaApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.adanvced_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdanvcedJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdanvcedJpaApplication.class, args);
+	}
+
+}
---
+++ hello/adanvced_jpa/src/test/java/com/springboot/adanvced_jpa/AdanvcedJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.adanvced_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdanvcedJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
